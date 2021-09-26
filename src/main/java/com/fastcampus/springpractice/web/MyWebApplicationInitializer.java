d42869e5584593ce1034f23e305bafde7fb0ac4f
seon54 <seonhyeongpark54@gmail.com>
2021-09-26 18:19:48 +0900

    #1 Add controller

---
+++ src/main/java/com/fastcampus/springpractice/web/MyWebApplicationInitializer.java
@@ -0,0 +1,28 @@
+package com.fastcampus.springpractice.web;
+
+import com.fastcampus.springpractice.config.Config;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+
+        // Load Spring web application configuration
+        DispatcherServlet servlet;
+        try (AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext()) {
+            context.register(Config.class);
+
+            // Create and register the DispatcherServlet
+            servlet = new DispatcherServlet(context);
+        }
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/app/*");
+    }
+}
